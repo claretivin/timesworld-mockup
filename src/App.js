ff57390e8c002de87ef05aa27ed3bcf28b35d741
ivinclaret99 <ivin@protorix.com>
2023-08-27 13:13:36 +0530

    page removed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Signin from "./components/Signin";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      {/* <Signin /> */}
       <Login />
     </div>
   );
